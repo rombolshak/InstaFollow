a59d999a8ff02fac62c993f4dd71cd62d543023c
Roman Bolshakov <rombolshak@gmail.com>
2013-06-22 21:24:11 +0400

    Release v1.0

---
+++ protected/commands/StopProcessCommand.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: roma
+ * Date: 17.05.13
+ * Time: 22:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+class StopProcessCommand {
+
+}
